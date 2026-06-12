dbfbf3447fb92218a4a150341c7a0b838748fffe
Dinesh Rajpurohit <dineshrajpurohitg25csa@vetias.ac.in>
2026-06-12 10:29:18 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+//variable decleration using var
+var name = 'dinesh'
+console.log(name)
+
+//variable decleration using let
+let age= 25
+console.log(age)
+
+//variable decleration using const        
+const country = 'India'
+console.log(country)
+
+
+
+
+
