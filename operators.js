6999885c699c65503d7af3675def15aedb15ff0d
Dinesh Rajpurohit <dineshrajpurohitg25csa@vetias.ac.in>
2026-06-12 11:17:18 +0530

    added operators

--- operators.js
+++ operators.js
@@ -6,3 +6,16 @@ console.log("Subtraction: " + (a - b)); // 5
 console.log("Multiplication: " + (a * b)); // 50
 console.log("Division: " + (a / b)); // 2
 console.log("Modulus: " + (a % b)); // 0
+
+//assignment operators
+var c = 20;
+c += 5; // c = c + 5
+console.log("After addition assignment: " + c); // 25
+c -= 3; // c = c - 3
+console.log("After subtraction assignment: " + c); // 22
+c *= 2; // c = c * 2
+console.log("After multiplication assignment: " + c); // 44
+c /= 4; // c = c / 4
+console.log("After division assignment: " + c); // 11
+c %= 3; // c = c % 3
+console.log("After modulus assignment: " + c); // 2
